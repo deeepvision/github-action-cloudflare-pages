93738187157bdfaf000a549f211e7357591899dd
Sergii Sadovyi <1175636+sergsadovyi@users.noreply.github.com>
2023-03-23 17:59:29 +0100

    feat: add npm auth

--- run.sh
+++ run.sh
@@ -5,6 +5,9 @@ set -e
 function main() {
   BUILD_COMMAND=${INPUT_BUILD:-build}
 
+  echo "@deeepvision:registry=https://npm.pkg.github.com" >> .npmrc && \
+  echo "//npm.pkg.github.com/:_authToken=${GITHUB_TOKEN}" >> .npmrc && \
+
   npm install
   npm run ${BUILD_COMMAND}
   wrangler pages publish --project-name=${INPUT_PROJECT} --branch master ${INPUT_DIRECTORY}
