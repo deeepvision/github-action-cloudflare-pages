9d283e33739ed9ff89e0885339a27f391723e7a8
Sergii Sadovyi <1175636+sergsadovyi@users.noreply.github.com>
2023-10-31 12:52:18 +0100

    feat: use "dist" as default directory

--- run.sh
+++ run.sh
@@ -4,13 +4,14 @@ set -e
 
 function main() {
   BUILD_COMMAND=${INPUT_BUILD:-build}
+  BUILD_DIRECTORY=${INPUT_DIRECTORY:-dist}
 
   echo "@deeepvision:registry=https://npm.pkg.github.com" >> .npmrc && \
   echo "//npm.pkg.github.com/:_authToken=${GITHUB_TOKEN}" >> .npmrc && \
 
   npm install
   npm run ${BUILD_COMMAND}
-  wrangler pages publish --project-name=${INPUT_PROJECT} --branch master ${INPUT_DIRECTORY}
+  wrangler pages publish --project-name=${INPUT_PROJECT} --branch master ${BUILD_DIRECTORY}
 }
 
 main
