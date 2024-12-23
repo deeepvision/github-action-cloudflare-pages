296d8eca7ec3f55e6f55ad5fbd37d83e0436febe
Vitaliy Angolenko <v.angolenko@deepvision.team>
2024-12-23 18:02:32 +0200

    feat: auth to @deep registry

--- run.sh
+++ run.sh
@@ -6,8 +6,9 @@ function main() {
   BUILD_COMMAND=${INPUT_BUILD:-build}
   BUILD_DIRECTORY=${INPUT_DIRECTORY:-dist}
 
-  echo "@deeepvision:registry=https://npm.pkg.github.com" >> .npmrc && \
-  echo "//npm.pkg.github.com/:_authToken=${GITHUB_TOKEN}" >> .npmrc && \
+  echo "@deep:registry=https://us-npm.pkg.dev/deepops/deep/" >> .npmrc && \
+  echo "//us-npm.pkg.dev/deepops/deep/:always-auth=true" >> .npmrc && \
+  echo "//us-npm.pkg.dev/deepops/deep/:_authToken=${NPM_TOKEN}" >> .npmrc && \
 
   npm install
   npm run ${BUILD_COMMAND}
