3a0b4cd3ddbf3cd879f1201d156a0a7424c4121f
Sergii Sadovyi <1175636+sergsadovyi@users.noreply.github.com>
2025-05-14 08:33:30 +0200

    fix: make NPM_TOKEN optional

--- run.sh
+++ run.sh
@@ -6,21 +6,13 @@ function main() {
   BUILD_COMMAND=${INPUT_BUILD:-build}
   BUILD_DIRECTORY=${INPUT_DIRECTORY:-dist}
 
-  # Debugging: Print if NPM_TOKEN is set
-  if [ -z "$NPM_TOKEN" ]; then
-    echo "Error: NPM_TOKEN is not set."
-    exit 1
-  else
-    echo "NPM_TOKEN is set."
+  # Check if NPM_TOKEN is set
+  if [ -n "$NPM_TOKEN" ]; then
+    echo "Configuring @deep registry."
+    echo "@deep:registry=https://us-npm.pkg.dev/deepops/deep/" >> .npmrc
+    echo "//us-npm.pkg.dev/deepops/deep/:always-auth=true" >> .npmrc
+    echo "//us-npm.pkg.dev/deepops/deep/:_authToken=${NPM_TOKEN}" >> .npmrc
   fi
-
-  echo "@deep:registry=https://us-npm.pkg.dev/deepops/deep/" >> .npmrc && \
-  echo "//us-npm.pkg.dev/deepops/deep/:always-auth=true" >> .npmrc && \
-  echo "//us-npm.pkg.dev/deepops/deep/:_authToken=${NPM_TOKEN}" >> .npmrc
-
-  # Debugging: Print the contents of .npmrc
-  echo "Contents of .npmrc:"
-  cat .npmrc
 
   npm install
   npm run ${BUILD_COMMAND}
